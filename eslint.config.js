65bb76d94f61e3c0c6d6b7811d3692cb2fd76eac
Valentin <valentin@waldeck.dev>
2026-06-06 17:14:14 +0200

    chore: add ESLint with Vue + TS rules and lint-on-save config

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+﻿import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+
+export default tseslint.config(
+  { ignores: ["dist/", "node_modules/"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
+  {
+    files: ["src/**/*.vue", "dev/**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        sourceType: "module",
+      },
+    },
+  },
+  {
+    files: ["src/**/*.{ts,vue}", "dev/**/*.{ts,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "warn",
+      "no-alert": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "vue/multi-word-component-names": "off",
+      "vue/require-default-prop": "warn",
+      "vue/no-unused-refs": "warn",
+    },
+  },
+);
